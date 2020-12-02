e5c797fa78c983ce328ad1850ecb83932809199f
João Agnelo <joaoa13portugal@gmail.com>
2020-12-02 05:02:36 +0000

    updated sumo cmd to start maximized and at the top-left corner of the display

--- src/globals.py
+++ src/globals.py
@@ -12,20 +12,15 @@ class Globals:
         "width": 800,
         "height": 600
     }
-    VNC_SUMO_FILES = ["gui-settings", "additional-files", "net-file", "route-files", "basecars-emission-by-edges-out",
-                      "edge-data-out"]
+    VNC_SUMO_FILES = ["gui-settings", "net-file", "route-files"]
     VNC_SUMO_CMD = "sumo-gui --gui-settings-file gui-settings.xml " \
-                   "--additional-files additional-files.xml " \
                    "--net-file net-file.xml " \
                    "--route-files route-files.xml " \
                    "--device.emissions.probability 1.0 " \
-                   "--emission-output.precision 6 " \
                    "--collision.action warn " \
                    "--time-to-teleport -1 " \
                    "--window-size $(xdpyinfo | awk '/dimensions/{print $2}' | awk '{gsub(\"x\", \",\")} {print}') " \
-                   "--window-pos 0,0"   # \
-                   # "--window-size %d,%d " % (VNC_RESOLUTION["width"], VNC_RESOLUTION["height"]) + \
-                   # "--window-pos 0,0"
+                   "--window-pos 0,0"
     BASE_WEBSOCKIFY_PORT = 6080
     BASE_DISPLAY_INDEX = 1
     POOL_BASE_SIZE = 10
